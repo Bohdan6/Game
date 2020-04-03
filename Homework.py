9460cb1ebdf2e7c7575aa5b1943b25eda06348a1
Bohdan6 <61345563+Bohdan6@users.noreply.github.com>
2020-04-03 17:28:48 +0200

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,30 @@
+#Task 1
+lst_1 = [x for x in range(0 , 101, 2)]
+print (lst_1)
+
+
+#Task 2
+lst_2 = [x for x in range( 57, 140, 2)]
+print(lst_2) 
+
+
+#Task 3
+lst_3 = [(x**2) for x in range(30)]
+print(lst_3)
+
+
+#Task 4
+lst_4 = [x for x in range(-100, 100)]
+print(lst_4)
+
+
+#Task 5
+lst_5 = [200 for x in range (87)]
+print(lst_5)
+
+
+#Task 6
+C = [x for x in range (101)]
+F = [int(x * 1.8 + 32) for x in C]
+
+print(C , F)
